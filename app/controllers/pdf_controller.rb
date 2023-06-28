f9d321dd87bf933466cf9d1274d81f3fdf730f5c
kasnino <16484156+kasnino@users.noreply.github.com>
2023-06-28 15:58:17 -0500

    mejoras en el nuevo css - eliminando flex y demas cosas

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -7,46 +7,62 @@ class PdfController < ApplicationController
   def generate
  
 #Informacion portada
-@city_ = "Medallo"
+@city_ = "Medellin"
 @country_ = "Venezuela"
-@zip_code = "10001"
-@apt_ = "5B"
-@member_id = "12345"
-@cig_id = "CIG1234"
-@ffm_id = "FFM5678"
-@email = "ejemplo@gmail.com"
-@mailing_adress = "Calle Principal"
-@phone = "555-1234-555"
+@zip_code = "1010"
+@apt_ = "3B"
+@member_id = "1234567890"
+@cig_id = "0987654321"
+@ffm_id = "1122334455"
+@email = "correo@example.com"
+@mailing_adress = "Av. Bolívar, Edif"
+@phone = "+58 212-555-1212"
 
 # client
-@name_client = "Juana"
-@m_name_client = "Arcos"
+@name_client = "Juan"
+@m_name_client = "Carlos"
 @last_name_client = "Pérez"
-@dob_client = "01/01/1990"
-@age_client = "31"
-@place_birth_client = "Ciudad de México"
-@categoria_client = "Platino"
+@dob_client = "01/01/1980"
+@age_client = 43
+@place_birth_client = "Maracaibo"
+@categoria_client = "A"
 @status_civil_client = "Casado"
-@driver_license_client = "12345678"
-@peso_client = "75 kg"
-@talla_client = "1.80 m"
-@ssn_client = "123-45-6789"
-@a_code_client = "A1234567"
-@card_client = "1234 5678 9012 3456"
-    
+@driver_license_client = "V-12345678"
+@peso_client = 75
+@talla_client = 175
+@ssn_client = "V-123456789"
+@a_code_client = "ABC123XYZ9"
+@card_client = "4111 1111 1111 1111"
+
 # Spouse
 @name_spouse = "María"
-@m_name_spouse = "Elena"
+@m_name_spouse = "Isabel"
 @last_name_spouse = "García"
-@dob_spouse = "02/02/1992"
-@age_spouse = "29"
+@dob_spouse = "02/02/1982"
+@age_spouse = 41
 @peso_spouse = "65 kg"
-@talla_spouse = "1.70 m"
-@ssn_spouse = "987-65-4321"
-@a_code_spouse = "A7654321"
-@card_spouse = "5678 9012 3456 7890"
+@talla_spouse = 165
+@ssn_spouse = "V-987654321"
+@a_code_spouse = "XYZ987ABC6"
+@card_spouse = "5500 0000 0000 0004"
 
-    
+#Bnak company    
+@income = 1000
+@employer = "Acme Inc."
+@work_type = "Full-time"
+@employer_phone = "+1 555-1234"
+@account_holder = "Juan Pérez"
+@bank = "Banco de Venezuela"
+@routing = "123456789"
+@account_number = "0123456789"
+@card = "master card"
+@exp = "12/25"
+@cvc = "123"
+@company = "Seguros Caracas"
+@effective_day = "01/01/2022"
+@plan_type = "Individual"
+@plan = "Plan Básico"
+@monthly_premium = 50
 
   # Aqui se puede agregar el Array de Objetos para iterar todos los elementos
   # En la carpeta public esta el JSON client_information de prueba
---
+++ config/initializers/wicked_pdf.rb
@@ -0,0 +1,4 @@
+WickedPdf.config = {
+  exe_path: '/usr/bin/wkhtmltopdf',
+  enable_local_file_access: true
+}
